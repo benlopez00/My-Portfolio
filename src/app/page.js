27a72d29471534ca55392ec69b270a6f8b6b3ad3
benlopez00 <lopezavilaben00@gmail.com>
2024-11-07 22:14:46 -0300

    mejorando estilos

--- src/app/page.js
+++ src/app/page.js
@@ -5,29 +5,48 @@ import Image from "next/image"
 
 export default function Component() {
 	return (
-		<div className="min-h-screen bg-white p-2 w-screen flex flex-col items-center">
-			<div className="grid w-10/12 grid-cols-8 gap-1">
-					<Card className="col-span-1 aspect-[1/1] bg-red-400">
-						<CardBody className="text-center">
-							<Typography variant="h3">
-								Hola
-							</Typography>
-						</CardBody>
-					</Card>
-					<Card className="col-span-3 aspect-[3/1] bg-green-400">
-						<CardBody className="text-center">
-							<Typography variant="h3">
-								Hola
-							</Typography>
-						</CardBody>
-					</Card>
-					<Card className="col-span-4 aspect-[1/1] bg-blue-400">
-						<CardBody className="text-center">
-							<Typography variant="h3">
-								Hola
-							</Typography>
-						</CardBody>
-					</Card>
+		<div className="min-h-screen p-2 w-screen flex flex-col items-center" style={{backgroundColor: 'rgb(247, 242, 242)'}}>
+			<div className="w-full h-10">
+
+			</div>
+			<div className="grid gap-6" style={{ gridTemplateColumns: 'repeat(4, 280px)', gridAutoRows: '280px' }}>
+				<Card className="col-span-2 w-full h-full bg-white flex items-center p-3">
+					<div className="">
+						<Image
+							src="/perfil_Blanco.png"
+							alt="Profile illustration"
+							width={100}
+							height={100}
+							className="bg-orange-500 object-cover"
+						/>
+					</div>
+					<CardBody className="text-center">
+						<Typography variant="h3">
+							Hola
+						</Typography>
+					</CardBody>
+				</Card>
+				<Card className="w-full h-full row-span-2 bg-green-400">
+					<CardBody className="text-center">
+						<Typography variant="h3">
+							Hola
+						</Typography>
+					</CardBody>
+				</Card>
+				<Card className="col-span-1 aspect-[1/1] bg-blue-400">
+					<CardBody className="text-center">
+						<Typography variant="h3">
+							Hola
+						</Typography>
+					</CardBody>
+				</Card>
+				<Card className="col-span-1 aspect-[1/1] bg-blue-400">
+					<CardBody className="text-center">
+						<Typography variant="h3">
+							Hola
+						</Typography>
+					</CardBody>
+				</Card>
 			</div>
 		</div>
 	)
