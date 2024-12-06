bd8c26b637a1b9e84e050efafa2adcbe6165681b
benlopez00 <lop.ben00@gmail.com>
2024-12-05 21:47:21 -0300

    cambios en el home y pagina de info personal

--- src/app/additional-info/page.js
+++ src/app/additional-info/page.js
@@ -4,68 +4,105 @@ import Link from 'next/link';
 
 export default function AdditionalInfoOverlay() {
 	return (
-		<div className="p-2 w-full flex flex-col items-center bg-light3 dark:bg-dark1 font-sans">
-			<div className="grid gap-6" style={{ gridTemplateColumns: 'repeat(4, 270px)', gridAutoRows: '270px' }}>
-				<Card className="card col-span-2 row-span-2 bg-light2 dark:bg-dark2 flex items-center p-3">
-					<CardBody className="p-0">
-						<Typography className="font-sans text-[rgb(31,31,31)] dark:text-[rgb(204,204,204)]">
-							- Rediseñar partes de la UI del Portal con ReactJS para mejorar la experiencia del usuario integrando bibliotecas como KendoUI y Bootstrap
-							- Actualizar e implementar nuevas funcionalidades del backend de ASP.NET Core MVC para cumplir con las necesidades de los clientes
-							- Implementar funcionalidades en Flutter utilizando Dart, conectando componentes móviles con sistemas backend en .NET a través de APIs RESTful.
-							- Utilizar LINQ y HeidiSQL para realizar consultas eficientes a la base de datos y mejorar el rendimiento del sistema, asegurando escalabilidad y optimización de datos.
-							- Trabajar en un entorno bajo metodologías Scrum, gestionando tareas en JIRA, participando en revisiones de código y aplicando mejores prácticas de desarrollo full-stack.
-							Este año en Adata no solo me enseñó sobre tecnología; me mostró lo gratificante que es trabajar creando software que realmente marcan una diferencia para las personas.
-						</Typography>
-					</CardBody>
-				</Card>
-				<Card className="card col-span-2 row-span-2 bg-light2 dark:bg-dark2 flex items-center p-3">
-					<CardBody className="p-0">
-						<Typography variant="h1" className="font-sans font-bold text-[rgb(31,31,31)] dark:text-[rgb(204,204,204)]">
+		<div className="p-2 w-full flex flex-col items-center bg-light3 dark:bg-dark1 font-sans bg-transparent">
+			<div className="grid gap-6 z-40" style={{ gridTemplateColumns: 'repeat(4, 270px)', gridAutoRows: '270px' }}>
+				<Card className="card col-span-2 row-span-2 p-8 bg-light2 dark:bg-dark2">
+					<CardBody className="p-0 h-full flex flex-col items-start gap-5">
+						<Typography variant="h2" className="font-sans font-medium text-light7 dark:text-dark7">
 							Sobre mi
 						</Typography>
-						<Typography className="font-sans text-[rgb(31,31,31)] dark:text-[rgb(204,204,204)]"> 
-							¡Hola! Soy un desarrollador **organizado, disciplinado y metódico**, apasionado por crear soluciones tecnológicas que sean tanto funcionales como estéticamente agradables. Mi camino en la programación comenzó con Arduinos y poco a poco fui construyendo habilidades en herramientas como **ReactJS** y **.NET Core**, que me permiten desarrollar interfaces intuitivas respaldadas por un backend sólido. Los videojuegos, las redes sociales y la curiosidad natural por entender cómo funcionan las cosas me llevaron al mundo de la tecnología. Me inspiran los diseños funcionales y eficaces, y trato de aplicar esos principios en todo lo que desarrollo, desde la lógica backend hasta la experiencia del usuario.
-						</Typography>
+						<div className="h-full flex flex-col justify-evenly">
+							<Typography variant="paragraph" className="font-sans font-light text-light6 dark:text-dark6">
+								Nací en Munich, Alemania, y fui criado en Argentina.
+							</Typography>
+							<Typography variant="paragraph" className="font-sans font-light text-light6 dark:text-dark6">
+								Desde pequeño me apasiona la tecnología y los libros de historia (y los autos).
+							</Typography>
+							<Typography variant="paragraph" className="font-sans font-light text-light6 dark:text-dark6">
+								Siempre me preguntaba como funcionaba el televisor que teníamos en el living o la radio del auto, el como las cosas iban evolucionando y mejorando.
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Mi adolescencia fue marcada por el internet y los videojuegos. Y en el club Interact de mi ciudad encontré mi lugar diseñando posts para el comité de relaciones publicas.
+							</Typography>
+						</div>
 					</CardBody>
 				</Card>
-				<Card className="card col-span-2 row-span-2 bg-light2 dark:bg-dark2 flex items-center p-3">
-					<CardBody className="p-0">
-						<Typography className="font-sans text-[rgb(31,31,31)] dark:text-[rgb(204,204,204)]"> 
-							Cuando no estoy programando, me encontrarás leyendo libros sobre economía, ciencia o historia, o disfrutando de la música de **AC/DC**, algo de rap alemán o incluso country americano. Me atraen las sociedades organizadas y funcionales como Alemania (¡y también me encanta el frío!). Creo firmemente que la funcionalidad debe ser la base de cualquier diseño, y que la creatividad debe complementar esa funcionalidad, no competir con ella. Mi objetivo siempre es facilitar la vida a las personas, ya sea resolviendo necesidades que no tenían solución o mejorando las opciones existentes con una experiencia de usuario de calidad.
-						</Typography>
+				<Card className="col-span-1 row-span-2 card bg-light2 dark:bg-dark2 overflow-hidden">
+					<img src="/berlinTvTower.jpg" alt="Big Ben" className="w-full h-full object-cover" />
+				</Card>
+				<Card className="col-span-1 row-span-2 card bg-light2 dark:bg-dark2 overflow-hidden">
+					<img src="/trainTracks.jpg" alt="Big Ben" className="w-full h-full object-cover" />
+				</Card>
+				<Card className="col-span-1 row-span-2 card bg-light2 dark:bg-dark2 overflow-hidden">
+					<img src="/bremenAerial.jpg" alt="Big Ben" className="w-full h-full object-cover" />
+				</Card>
+				<Card className="col-span-1 row-span-2 card bg-light2 dark:bg-dark2 overflow-hidden">
+					<img src="/bremenNight.jpg" alt="Big Ben" className="w-full h-full object-cover" />
+				</Card>
+				<Card className="col-span-2 row-span-2 card p-8 bg-light2 dark:bg-dark2">
+					<CardBody className="p-0 h-full">
+						<div className="h-full flex flex-col justify-evenly">
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Fue a través de Interact que participe de un intercambio estudiantil en Alemania, allí estuve 1 año en Bremen.
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Ya de vuelta en Tucuman y en la universidad, la pandemia me encontró sin mucho animo estudiando economía. Fue entonces que comencé a adentrarme en la programación y el diseño de interfaces
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								A través de cursos y mucho aprendizaje por mi cuenta, fui explorando React, .NET y diseño UI, áreas que rápidamente se convirtieron en mi pasión y en el enfoque de mi desarrollo profesional.
+							</Typography>
+						</div>
 					</CardBody>
 				</Card>
-				<Card className="col-span-2 row-span-1 bg-light2 dark:bg-dark2 card p-4 flex items-center justify-center">
-					<Link href="/">
-						<Button size="lg" className="bg-blue-500 text-white hover:bg-blue-600">
-							Volver
-						</Button>
-					</Link>
-				</Card>
-				
-				<Card className="card col-span-2 row-span-2 bg-light2 dark:bg-dark2 flex items-center p-3">
-					<CardBody className="p-0">
-						<Typography variant="h1" className="font-sans font-bold text-light7 dark:text-dark7">
-							Experiencia
-						</Typography>
-						<Typography variant="h2" className="font-sans font-medium text-light7 dark:text-dark7">
-							Adata Software Gmbh (Jul '23 – Jun '24)
-						</Typography>
-						<Typography className="font-sans text-light7 dark:text-dark7">
-							Entre el 2023 y el 2024 viví en Alemania y en ese tiempo trabajé un año para Adata de forma presencial en Verden (Baja Sajonia). Allí trabajé junto a 2 desarrolladores en el desarrollo y soporte del Mitarbeiter Portal. ¿Qué es el Mitarbeiter Portal? Es una herramienta integral de recursos humanos que ayuda a pequeñas y medianas empresas a gestionar su personal de manera eficiente. Desde administrar horarios hasta facilitar la comunicación interna, este portal es el corazón organizativo para muchos negocios. Durante ese tiempo, me enfrenté a desafíos técnicos y creativos, pero cada tarea me dejó una enorme satisfacción. Algunas de las contribuciones que hice son:
-						</Typography>
+				<Card className="col-span-2 row-span-2 card p-8 bg-light2 dark:bg-dark2">
+					<CardBody className="p-0 h-full flex flex-col items-start gap-5">
+						<div>
+							<Typography variant="h3" className="font-sans font-medium text-light7 dark:text-dark7">
+								Adata Software Gmbh
+							</Typography>
+							<Typography variant="h5" className="font-sans font-medium text-light5 dark:text-dark5">
+								Jul '23 – Jun '24
+							</Typography>
+						</div>
+						<div className="h-full flex flex-col justify-evenly">
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Deseoso de visitar nuevamente Alemania, aplique a una visa working-holiday. Allí trabaje durante un año en Adata Software GmbH en Verden, Baja Sajonia.
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Me dediqué al desarrollo y soporte del Mitarbeiter Portal, un software de recursos humanos que ayuda a pequeñas y medianas empresas a gestionar su personal.
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Durante ese tiempo, me enfrenté a desafíos técnicos y creativos, pero cada tarea me dejó una enorme satisfacción.
+							</Typography>
+						</div>
 					</CardBody>
-				</Card>
-				<Card className="col-span-2 row-span-2 bg-light2 dark:bg-dark2 card p-4">
-					<CardBody>
-						<Typography variant="h1" className="font-sans font-bold text-light7 dark:text-dark7">
+				</Card>				
+				<Card className="col-span-2 row-span-2 card p-8 bg-light2 dark:bg-dark2">
+					<CardBody className="p-0 h-full flex flex-col items-start gap-5">
+						<Typography variant="h3" className="font-sans font-medium text-light7 dark:text-dark7">
 							Mi objetivo actual
 						</Typography>
-						<Typography className="font-sans text-light7 dark:text-dark7">
-							Estoy enfocado en seguir aprendiendo en las áreas donde necesito más preparación, como el despliegue y la publicación de sitios completos. Mi meta a corto plazo es dominar el proceso de hosting y publicación de aplicaciones, mientras refuerzo mis habilidades en **.NET** y **ReactJS**. A largo plazo, aspiro a liderar equipos de desarrollo y guiar a otros desarrolladores juniors, porque creo que mi experiencia y enfoque pueden marcar una diferencia en su crecimiento profesional.
-						</Typography>
+						<div className="h-full flex flex-col justify-evenly">
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Estoy enfocado en seguir aprendiendo en las áreas donde necesito más preparación, como el despliegue y la publicación de sitios completos.
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								Mi meta a corto plazo es dominar el proceso de hosting y publicación de aplicaciones, mientras refuerzo mis habilidades en .NET y ReactJS.
+							</Typography>
+							<Typography variant="medium" className="font-sans font-light text-light6 dark:text-dark6">
+								A largo plazo, aspiro a liderar equipos de desarrollo y guiar a otros desarrolladores juniors, porque creo que mi experiencia y enfoque pueden marcar una diferencia en su crecimiento profesional.
+							</Typography>
+						</div>
 					</CardBody>
 				</Card>
+				<Link href="/" className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
+					<Button
+						size="lg"
+						className="bg-darkPrimary text-white hover:bg-green-500"
+					>
+						Volver
+					</Button>
+				</Link>
 			</div>
 		</div>
 	);
--- src/app/page.js
+++ src/app/page.js
@@ -7,66 +7,49 @@ import Link from 'next/link';
 
 export default function Component() {
 	return (
-		<div className="p-2 w-full flex flex-col items-center bg-light3 dark:bg-dark1 font-sans">
-			<div className="grid gap-6" style={{ gridTemplateColumns: 'repeat(4, 270px)', gridAutoRows: '270px' }}>
+		<div className="p-2 w-full flex flex-col items-center font-sans bg-transparent">
+			<div className="grid gap-6 z-20" style={{ gridTemplateColumns: 'repeat(4, 270px)', gridAutoRows: '270px' }}>
 				<Card className="col-span-1 row-span-1 card bg-light2 dark:bg-dark2">
 					<img src="/Big_Ben.jpg" alt="Big Ben" className="w-full h-full object-cover card" />
 				</Card>
 				<Card className="col-span-1 row-span-1 card p-8 bg-light2 dark:bg-dark2">
-					<CardBody className="p-0 h-full flex flex-col items-start gap-10">
+					<CardBody className="p-0 h-full flex flex-col items-start justify-between">
 						<div>
-							<Typography variant="h4" className="font-sans font-medium text-light7 dark:text-dark7">
+							<Typography variant="h4" className="font-sans font-normal text-light7 dark:text-dark7">
 								¡Hola🖖!
 							</Typography>
 						</div>
 						<div>
-							<Typography variant="h2" className="font-sans font-medium text-light7 dark:text-dark7">
+							<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
 								Soy Ben
 							</Typography>
 						</div>
 						<div>
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
+							<Typography variant="small" className="font-sans text-[16px] text-light7 dark:text-dark7">
 								Desarrollador Full Stack
 							</Typography>
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								🌐Tucuman, Argentina
+							<Typography variant="small" className="font-sans text-[16px] text-light7 dark:text-dark7">
+								Diseñador UI
 							</Typography>
 						</div>
 					</CardBody>
 				</Card>
 				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
 					<CardBody className="p-0 h-full flex flex-col items-start gap-9">
-						<Typography variant="h2" className="font-sans font-medium text-light7 dark:text-dark7">
+						<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
 							Mi Stack
 						</Typography>
 						<TechCarousel/>
 					</CardBody>
 				</Card>
 				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
-					<CardBody className="p-0 h-full flex flex-col items-start gap-10">
-						<div className="w-full flex space justify-between">
-							<Typography variant="h2" className="font-sans font-medium text-light7 dark:text-dark7">
-								Mas sobre mi
+					<CardBody className="p-0 h-full flex flex-col items-center">
+						<div className="w-11/12 h-full flex flex-col justify-around">
+							<Typography className="font-sans text-[16px] font-normal text-light7 dark:text-dark7">
+								Soy desarrollador web especializado en .NET, React y Flutter, apasionado por crear soluciones innovadoras que combinan funcionalidad y diseño.
 							</Typography>
-							<Link href="/additional-info">
-								<Button variant="text" size="sm" className="bg-light2 dark:bg-dark2 p-5 text-white hover:bg-darkPrimary dark:hover:bg-darkPrimary  group">
-									<img
-										src="/logos/open-in-browser-svgrepo-com.svg"
-										alt="Open Tab"
-										className="w-6 h-6 filter group-hover:invert dark:invert"
-									/>
-								</Button>
-							</Link>
-						</div>
-						<div className="flex flex-col gap-2">
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								💼 Cuento con experiencia en .NET, ReactJS y Flutter
-							</Typography>
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								🚦 Desde programar Arduinos hasta trabajar en Alemania
-							</Typography>
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								📈 Siempre buscando mejorar y aprender algo nuevo
+							<Typography className="font-sans text-[16px] font-normal text-light7 dark:text-dark7">
+								Con experiencia en proyectos frontend y backend, disfruto trabajando en equipo y superando retos tecnológicos para entregar productos de alta calidad.
 							</Typography>
 						</div>
 					</CardBody>
@@ -109,14 +92,14 @@ export default function Component() {
 							/>
 						</a>
 					</Card>
-				</Card>
+				</Card>	
 				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
 					<CardBody className="p-0 h-full flex flex-col items-start gap-10">
 						<div className="w-full flex space justify-between">
-							<Typography variant="h2" className="font-sans font-medium text-light7 dark:text-dark7">
-								Mi experiencia
+							<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
+								Mas sobre mi
 							</Typography>
-							<Link href="/experiencia">
+							<Link href="/additional-info">
 								<Button variant="text" size="sm" className="bg-light2 dark:bg-dark2 p-5 text-white hover:bg-darkPrimary dark:hover:bg-darkPrimary  group">
 									<img
 										src="/logos/open-in-browser-svgrepo-com.svg"
@@ -126,17 +109,35 @@ export default function Component() {
 								</Button>
 							</Link>
 						</div>
-						<div className="flex flex-col gap-2">
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								🥨 Un año trabajando en herramientas de RRHH para empresas.
-							</Typography>
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								💎 Mejoré la UX rediseñando interfaces clave.
-							</Typography>
-							<Typography variant="small" className="font-sans font-light text-light7 dark:text-dark7">
-								💻 Implementé y actualicé funcionalidades en frontend, backend y móviles.
+						<Typography variant="paragraph" className="font-sans text-[16px] text-light7 dark:text-dark7">
+							🚦 Desde programar Arduinos hasta trabajar en Alemania
+						</Typography>
+					</CardBody>
+				</Card>
+				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
+					<CardBody className="p-0 h-full flex flex-col justify-between">
+						<div className="w-full flex space justify-between">
+							<Typography variant="h2" className="font-nyght-serif font-[500] text-light7 dark:text-dark7">
+								Mi experiencia
 							</Typography>
 						</div>
+						<Link href="/experiencia" passHref>
+							<div className="w-full flex flex-row items-center justify-between card bg-light3 dark:bg-dark4 group relative cursor-pointer rounded-lg">
+								<div className="h-full flex flex-col justify-around card p-8">
+									<Typography className="font-sans text-[20px] font-semibold text-light7 dark:text-dark7">
+										Adata Software GmbH
+									</Typography>
+									<Typography className="font-sans text-[16px] text-light7 dark:text-dark7">
+										Desarrollador ASP.NET Core
+									</Typography>
+									<Typography className="font-sans text-[14px] text-light6 dark:text-dark6">
+										July 2023 - June 2024
+									</Typography>
+								</div>
+								<img src="/logos/open-in-browser-svgrepo-com.svg" alt="Open Tab" className="w-6 h-6 mr-8 filter dark:invert transition-transform duration-300 group-hover:translate-y-[-5px] group-hover:invert[rgb(76,175,80)]"/>
+								<div className="absolute inset-0 card transition-shadow duration-300 group-hover:shadow-[inset_0_0_18px_5px_rgba(76,175,80,0.5)] group-active:shadow-[inset_0_0_30px_15px_rgba(76,175,80,0.9)] pointer-events-none"></div>
+							</div>
+						</Link>
 					</CardBody>
 				</Card>
 			</div>
