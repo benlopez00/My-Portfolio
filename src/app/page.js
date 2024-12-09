439592854800072dff6e8f03099010863da5c6e0
benlopez00 <lop.ben00@gmail.com>
2024-12-09 19:02:15 -0300

    text display adjusted

--- src/app/page.js
+++ src/app/page.js
@@ -35,18 +35,18 @@ export default function Component() {
 					</CardBody>
 				</Card>
 				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
-					<CardBody className="p-0 h-full flex flex-col items-start gap-9">
+					<CardBody className="p-0 h-full flex flex-col items-start gap-8">
 						<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
 							Mi Stack
 						</Typography>
 						<TechCarousel/>
 					</CardBody>
 				</Card>
-				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
+				<Card className="col-span-2 row-span-1 card py-8 px-12 bg-light2 dark:bg-dark2">
 					<CardBody className="p-0 h-full flex flex-col items-center">
-						<div className="w-11/12 h-full flex flex-col justify-around">
+						<div className="w-full h-full flex flex-col text-justify justify-around">
 							<Typography className="font-sans text-[16px] font-normal text-light7 dark:text-dark7">
-								Soy desarrollador web especializado en .NET, React y Flutter, apasionado por crear soluciones innovadoras que combinan funcionalidad y diseño.
+								Soy desarrollador web especializado en .NET, React y Flutter. Apasionado por crear soluciones innovadoras que combinan funcionalidad y diseño.
 							</Typography>
 							<Typography className="font-sans text-[16px] font-normal text-light7 dark:text-dark7">
 								Con experiencia en proyectos frontend y backend, disfruto trabajando en equipo y superando retos tecnológicos para entregar productos de alta calidad.
@@ -109,7 +109,7 @@ export default function Component() {
 							<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
 								Mi experiencia
 							</Typography>
-							<div className="w-32 h-8 rounded-2xl p-1 bg-light4 dark:bg-dark4 flex flex-row justify-around items-center">
+							<div className="w-32 h-10 rounded-3xl relative bottom-[3px] p-1 bg-light4 dark:bg-dark4 flex flex-row justify-around items-center">
 								<Typography className="font-sans text-[14px] font-normal text-green-500">
 									Disponible
 								</Typography>
@@ -120,7 +120,7 @@ export default function Component() {
 							</div>
 						</div>
 						<Link href="/experiencia" passHref>
-							<div className="w-full flex flex-row items-center justify-between card bg-light3 dark:bg-dark4 group relative cursor-pointer rounded-lg">
+							<div className="w-full flex flex-row items-center justify-between rounded-3xl bg-light3 dark:bg-dark4 group sticky cursor-pointer">
 								<div className="h-full flex flex-col justify-around p-8">
 									<Typography className="font-sans text-[20px] font-semibold text-light7 dark:text-dark7">
 										Adata Software GmbH
@@ -135,7 +135,7 @@ export default function Component() {
 								<div className="h-full flex flex-col justify-around p-8">
 									<ReactSVG src="/iconos/OpenTab.svg" className="w-6 h-6 fill-light7 dark:fill-dark7 transition-fill duration-300 group-hover:fill-green-500 group-hover:-translate-y-1"/>
 								</div>
-								<div className="absolute inset-0 card transition-shadow duration-300 group-hover:shadow-[inset_0px_29px_18px_-20px_rgba(76,175,80,0.5)] pointer-events-none"></div>
+								<div className="absolute inset-0 rounded-3xl transition-shadow duration-300 group-hover:shadow-[inset_0px_29px_18px_-20px_rgba(76,175,80,0.5)] pointer-events-none"></div>
 							</div>
 						</Link>
 					</CardBody>
