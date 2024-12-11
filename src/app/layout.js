070ecf243ca883ea0fce5a4746343cf5eb382060
benlopez00 <lop.ben00@gmail.com>
2024-12-10 22:56:06 -0300

    Optimized and prettier applied

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,50 +1,83 @@
-"use client";
-import { dmSans } from './fonts/dmSans'
-import "./global.css";
-import "maplibre-gl/dist/maplibre-gl.css";
-import BotonEstilo from "./components/BotonEstilo";
-import { Typography, Button, } from "@material-tailwind/react";
-import Image from "next/image";
+'use client';
+import { memo, useCallback, useEffect } from 'react';
+import { dmSans } from './fonts/dmSans';
+import './global.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import dynamic from 'next/dynamic';
+import { Typography, Button } from '@material-tailwind/react';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { motion, AnimatePresence } from 'framer-motion';
 import Link from 'next/link';
-import { useEffect } from 'react';
 
+const BotonEstilo = dynamic(() => import('./components/BotonEstilo'), {
+	ssr: false,
+});
 
+const LanguageButton = memo(({ src, alt }) => (
+	<Button size="sm" variant="text">
+		<Image src={src} alt={alt} width={22} height={22} />
+	</Button>
+));
 
-export default function RootLayout({ children }) {
+const Layout = ({ children }) => {
+	const pathname = usePathname();
+
 	useEffect(() => {
 		document.body.classList.add('vsc-initialized');
 	}, []);
-	const pathname = usePathname();
+
+	const memoizedChildren = useCallback(() => children, [children]);
+
 	return (
 		<html lang="en" className={dmSans.variable}>
-			<body className='bg-light3 dark:bg-dark1'>
+			<body className="bg-light3 dark:bg-dark1">
 				<nav className="w-full h-20 flex flex-col items-center justify-center">
 					<div className="w-3/4 flex flex-row justify-between items-center z-20">
-						<div className='w-44 flex justify-start'>
-							<BotonEstilo/>					
+						<div className="w-44 flex justify-start">
+							<BotonEstilo />
 						</div>
 						<div>
-						<Link href="/">
-							<Typography variant="h1" className="font-nyght-serif font-bold italic text-light7 dark:text-dark7">
-								BEN LOPEZ
-							</Typography>
-						</Link>
+							<Link href="/">
+								<Typography
+									variant="h1"
+									className="font-nyght-serif font-bold italic text-light7 dark:text-dark7"
+								>
+									BEN LOPEZ
+								</Typography>
+							</Link>
 						</div>
-						<div className='w-44 flex justify-end'>
-							<Button size="sm" variant="text"><Image src="/spain-svgrepo-com.svg" alt="Español" width={22} height={22}/></Button>
-							<Button size="sm" variant="text"><Image src="/united-kingdom-uk-svgrepo-com.svg" alt="English" width={22} height={22}/></Button>
-							<Button size="sm" variant="text"><Image src="/germany-svgrepo-com.svg" alt="Deutsch" width={22} height={22}/></Button>
+						<div className="w-44 flex justify-end">
+							<LanguageButton
+								src="/spain-svgrepo-com.svg"
+								alt="Español"
+							/>
+							<LanguageButton
+								src="/united-kingdom-uk-svgrepo-com.svg"
+								alt="English"
+							/>
+							<LanguageButton
+								src="/germany-svgrepo-com.svg"
+								alt="Deutsch"
+							/>
 						</div>
 					</div>
 				</nav>
 				<div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-[600px] overflow-hidden z-0">
-					<div className="absolute bg-green-500 dark:bg-green-400 opacity-90 dark:opacity-70 blur-[80px] rounded-full w-[420px] h-[310px] animate-gradientMove animate-squareMove" style={{ top: '-100px', left: '400px' }}></div>
-					<div className="absolute bg-blue-500 dark:bg-blue-400 opacity-90 dark:opacity-70 blur-[80px] rounded-full w-[400px] h-[310px] animate-gradientMove animate-triangularMove" style={{ top: '-100px', right: '400px' }}></div>
+					<div
+						className="absolute bg-green-500 dark:bg-green-400 opacity-90 dark:opacity-70 blur-[80px] rounded-full w-[420px] h-[310px] animate-gradientMove animate-squareMove"
+						style={{ top: '-100px', left: '400px' }}
+					></div>
+					<div
+						className="absolute bg-blue-500 dark:bg-blue-400 opacity-90 dark:opacity-70 blur-[80px] rounded-full w-[400px] h-[310px] animate-gradientMove animate-triangularMove"
+						style={{ top: '-100px', right: '400px' }}
+					></div>
 				</div>
 				<AnimatePresence mode="wait">
-					<div className='bg-transparent' style={{ position: 'relative', overflow: 'hidden' }}> 
+					<div
+						className="bg-transparent"
+						style={{ position: 'relative', overflow: 'hidden' }}
+					>
 						<motion.div
 							key={pathname}
 							initial={{ opacity: 0, y: 50 }}
@@ -52,12 +85,14 @@ export default function RootLayout({ children }) {
 							transition={{ duration: 0.5 }}
 							style={{ width: '100%', height: '100%' }}
 						>
-							<main>{children}</main>
+							<main>{memoizedChildren()}</main>
 						</motion.div>
 					</div>
 				</AnimatePresence>
-				<footer className='w-full h-20'></footer>
+				<footer className="w-full h-20"></footer>
 			</body>
 		</html>
-	)
-}
+	);
+};
+
+export default memo(Layout);
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,104 @@
 'use client';
-import { Card, CardBody, Typography, Button, } from "@material-tailwind/react";
-import TechCarousel from "./components/TechCarousel";
-import LocationMap from './components/LocationMap.js';
+import { memo, useMemo } from 'react';
+import { Card, CardBody, Typography } from '@material-tailwind/react';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import { ReactSVG } from 'react-svg';
 
+const TechCarousel = dynamic(() => import('./components/TechCarousel'), {
+	ssr: false,
+});
+const LocationMap = dynamic(() => import('./components/LocationMap'), {
+	ssr: false,
+});
+
+const SocialLink = memo(({ href, icon, gradient }) => (
+	<Card
+		shadow={false}
+		className={`rounded-3xl flex items-center justify-center bg-light0 dark:bg-dark4 transition-all ease-in-out duration-300 ${gradient}`}
+	>
+		<a
+			href={href}
+			target="_blank"
+			rel="noopener noreferrer"
+			className="w-full h-full flex justify-center items-center rounded-xl group"
+		>
+			<ReactSVG
+				src={icon}
+				className="w-12 h-12 fill-light6 dark:fill-dark7 transition-fill duration-300 group-hover:fill-white"
+			/>
+		</a>
+	</Card>
+));
+
+const InfoCard = memo(({ title, children }) => (
+	<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
+		<CardBody className="p-0 h-full flex flex-col items-start gap-8">
+			<Typography
+				variant="h2"
+				className="font-nyght-serif font-[500] text-light7 dark:text-dark7"
+			>
+				{title}
+			</Typography>
+			{children}
+		</CardBody>
+	</Card>
+));
+
 export default function Component() {
+	const socialLinks = useMemo(
+		() => [
+			{
+				href: 'https://example.com/resume',
+				icon: '/iconos/DocLogo.svg',
+				gradient: 'hover:bg-green-400 hover:dark:bg-green-400',
+			},
+			{
+				href: 'https://linkedin.com/in/ebenlopezavila00/',
+				icon: '/iconos/LinkedinLogo.svg',
+				gradient: 'hover:bg-blue-600 hover:dark:bg-blue-600',
+			},
+			{
+				href: 'mailto:lopezavilaben00@gmail.com',
+				icon: '/iconos/MailLogo.svg',
+				gradient: 'hover:bg-amber-500 hover:dark:bg-amber-500',
+			},
+			{
+				href: 'https://instagram.com/ben_fotos00/',
+				icon: '/iconos/InstaLogo.svg',
+				gradient: 'instagram-gradient',
+			},
+		],
+		[]
+	);
+
 	return (
 		<div className="p-2 w-full flex flex-col items-center font-sans bg-transparent">
-			<div className="grid gap-6 z-20" style={{ gridTemplateColumns: 'repeat(4, 270px)', gridAutoRows: '270px' }}>
-				<Card className="col-span-1 row-span-1 card bg-light2 dark:bg-dark2">
-					<img src="/Big_Ben.jpg" alt="Big Ben" className="w-full h-full object-cover card" />
+			<div
+				className="grid gap-6 z-20"
+				style={{
+					gridTemplateColumns: 'repeat(4, 270px)',
+					gridAutoRows: '270px',
+				}}
+			>
+				<Card className="col-span-1 row-span-1 card bg-light2 dark:bg-dark2 overflow-hidden">
+					<Image src="/Big_Ben.webp" alt="Ben Lopez Profile Photo" fill objectFit="cover"/>
 				</Card>
 				<Card className="col-span-1 row-span-1 card p-8 bg-light2 dark:bg-dark2">
 					<CardBody className="p-0 h-full flex flex-col items-start justify-between">
-						<div>
-							<Typography variant="h4" className="font-sans font-normal text-light7 dark:text-dark7">
-								¡Hola🖖!
-							</Typography>
-						</div>
-						<div>
-							<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
-								Soy Ben
-							</Typography>
-						</div>
+						<Typography
+							variant="h4"
+							className="font-sans font-normal text-light7 dark:text-dark7"
+						>
+							¡Hola🖖!
+						</Typography>
+						<Typography
+							variant="h2"
+							className="font-nyght-serif font-[500] text-light7 dark:text-dark7"
+						>
+							Soy Ben
+						</Typography>
 						<div>
 							<Typography className="font-sans text-[16px] text-light7 dark:text-dark7">
 								Desarrollador Full Stack
@@ -34,69 +109,70 @@ export default function Component() {
 						</div>
 					</CardBody>
 				</Card>
-				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
-					<CardBody className="p-0 h-full flex flex-col items-start gap-8">
-						<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
-							Mi Stack
-						</Typography>
-						<TechCarousel/>
-					</CardBody>
-				</Card>
+				<InfoCard title="Mi Stack">
+					<TechCarousel />
+				</InfoCard>
 				<Card className="col-span-2 row-span-1 card py-8 px-12 bg-light2 dark:bg-dark2">
 					<CardBody className="p-0 h-full flex flex-col items-center">
 						<div className="w-full h-full flex flex-col text-justify justify-around">
 							<Typography className="font-sans text-[16px] font-normal text-light7 dark:text-dark7">
-								Soy desarrollador web especializado en .NET, React y Flutter. Apasionado por crear soluciones innovadoras que combinan funcionalidad y diseño.
+								Soy desarrollador web especializado en .NET,
+								React y Flutter. Apasionado por crear soluciones
+								innovadoras que combinan funcionalidad y diseño.
 							</Typography>
 							<Typography className="font-sans text-[16px] font-normal text-light7 dark:text-dark7">
-								Con experiencia en proyectos frontend y backend, disfruto trabajando en equipo y superando retos tecnológicos para entregar productos de alta calidad.
+								Con experiencia en proyectos frontend y backend,
+								disfruto trabajando en equipo y superando retos
+								tecnológicos para entregar productos de alta
+								calidad.
 							</Typography>
 						</div>
 					</CardBody>
 				</Card>
-				<LocationMap/>
-				<Card className="col-span-1 row-span-1 card p-4 bg-light2 dark:bg-dark2 grid gap-5 justify-center content-center" style={{ gridTemplateColumns: 'repeat(2, 100px)', gridTemplateRows: 'repeat(2, 100px)' }}>
-					<Card shadow={false} className="rounded-3xl flex items-center justify-center bg-light0 dark:bg-dark4 transition-all ease-in-out duration-300 hover:bg-green-400 hover:dark:bg-green-400">
-						<a href="https://example.com/resume" target="_blank" rel="noopener noreferrer" className="w-full h-full flex justify-center items-center rounded-xl group">
-							<ReactSVG src="/iconos/DocLogo.svg" className="w-12 h-12 fill-light6 dark:fill-dark7 transition-fill duration-300 group-hover:fill-white"/>
-						</a>
-					</Card>
-					<Card shadow={false} className="rounded-3xl flex items-center justify-center bg-light0 dark:bg-dark4 transition-all ease-in-out duration-300 hover:bg-blue-600 hover:dark:bg-blue-600">
-						<a href="https://linkedin.com/in/ebenlopezavila00/" target="_blank" rel="noopener noreferrer" className="w-full h-full flex justify-center items-center rounded-xl group">
-							<ReactSVG src="/iconos/LinkedinLogo.svg" className="w-12 h-12 fill-light6 dark:fill-dark7 transition-fill duration-300 group-hover:fill-white"/>
-						</a>
-					</Card>
-					<Card shadow={false} className="rounded-3xl flex items-center justify-center bg-light0 dark:bg-dark4 transition-all ease-in-out duration-300 hover:bg-amber-500 hover:dark:bg-amber-500">
-						<a href="mailto:lopezavilaben00@gmail.com" className="w-full h-full flex justify-center items-center rounded-xl group">
-							<ReactSVG src="/iconos/MailLogo.svg" className="w-12 h-12 fill-light6 dark:fill-dark7 transition-fill duration-300 group-hover:fill-white"/>
-						</a>
-					</Card>
-					<Card shadow={false} className="rounded-3xl flex items-center justify-center bg-light0 dark:bg-dark4 instagram-gradient">
-						<a href="https://instagram.com/ben_fotos00/" target="_blank" rel="noopener noreferrer" className="w-full h-full flex justify-center items-center rounded-xl group">
-							<ReactSVG src="/iconos/InstaLogo.svg" className="w-12 h-12 fill-light6 dark:fill-dark7 transition-fill duration-300 group-hover:fill-white"/>
-						</a>
-					</Card>
-				</Card>	
+				<LocationMap />
+				<Card
+					className="col-span-1 row-span-1 card p-4 bg-light2 dark:bg-dark2 grid gap-5 justify-center content-center"
+					style={{
+						gridTemplateColumns: 'repeat(2, 100px)',
+						gridTemplateRows: 'repeat(2, 100px)',
+					}}
+				>
+					{socialLinks.map((link, index) => (
+						<SocialLink key={index} {...link} />
+					))}
+				</Card>
 				<Card className="col-span-2 row-span-1 card bg-light2 dark:bg-dark2 overflow-hidden">
-					<Link href="/additional-info" className="w-full h-full card p-8 group">
+					<Link
+						href="/additional-info"
+						className="w-full h-full card p-8 group"
+					>
 						<CardBody className="p-0 h-full flex flex-col items-start justify-between">
 							<div className="w-full flex justify-between items-center">
-								<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
+								<Typography
+									variant="h2"
+									className="font-nyght-serif font-[500] text-light7 dark:text-dark7"
+								>
 									Más sobre mí
 								</Typography>
 								<div className="rounded-full p-3">
-									<ReactSVG src="/iconos/OpenTab.svg" className="w-6 h-6 fill-light7 dark:fill-dark7 transition-fill duration-300 group-hover:fill-green-500 group-hover:-translate-y-1"/>
+									<ReactSVG
+										src="/iconos/OpenTab.svg"
+										className="w-6 h-6 fill-light7 dark:fill-dark7 transition-fill duration-300 group-hover:fill-green-500 group-hover:-translate-y-1"
+									/>
 								</div>
 							</div>
 							<div className="w-full flex flex-col items-start justify-between gap-2 mb-5">
 								<Typography className="font-sans text-[16px] text-light7 dark:text-dark7">
-									🚦 Desde programar Arduinos hasta trabajar en Alemania.
+									🚦 Desde programar Arduinos hasta trabajar
+									en Alemania.
 								</Typography>
 								<Typography className="font-sans text-[16px] text-light7 dark:text-dark7">
-									🔍 Me encanta encontrar la mejor solución para cada desafío.
+									🔍 Me encanta encontrar la mejor solución
+									para cada desafío.
 								</Typography>
 								<Typography className="font-sans text-[16px] text-light7 dark:text-dark7">
-									📈 Siempre buscando mejorar y aprender algo nuevo.
+									📈 Siempre buscando mejorar y aprender algo
+									nuevo.
 								</Typography>
 							</div>
 						</CardBody>
@@ -105,8 +181,11 @@ export default function Component() {
 				</Card>
 				<Card className="col-span-2 row-span-1 card p-8 bg-light2 dark:bg-dark2">
 					<CardBody className="p-0 h-full flex flex-col justify-between">
-					<div className="w-full flex justify-between items-center">
-							<Typography variant="h2" className="font-nyght-serif font-[500]  text-light7 dark:text-dark7">
+						<div className="w-full flex justify-between items-center">
+							<Typography
+								variant="h2"
+								className="font-nyght-serif font-[500] text-light7 dark:text-dark7"
+							>
 								Mi experiencia
 							</Typography>
 							<div className="w-32 h-10 rounded-3xl relative bottom-[3px] p-1 bg-light4 dark:bg-dark4 flex flex-row justify-around items-center">
@@ -133,7 +212,10 @@ export default function Component() {
 									</Typography>
 								</div>
 								<div className="h-full flex flex-col justify-around p-8">
-									<ReactSVG src="/iconos/OpenTab.svg" className="w-6 h-6 fill-light7 dark:fill-dark7 transition-fill duration-300 group-hover:fill-green-500 group-hover:-translate-y-1"/>
+									<ReactSVG
+										src="/iconos/OpenTab.svg"
+										className="w-6 h-6 fill-light7 dark:fill-dark7 transition-fill duration-300 group-hover:fill-green-500 group-hover:-translate-y-1"
+									/>
 								</div>
 								<div className="absolute inset-0 rounded-3xl transition-shadow duration-300 group-hover:shadow-[inset_0px_29px_18px_-20px_rgba(76,175,80,0.5)] pointer-events-none"></div>
 							</div>
@@ -142,5 +224,5 @@ export default function Component() {
 				</Card>
 			</div>
 		</div>
-	)
+	);
 }
